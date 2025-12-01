4cc93a68f0fbc965ccdd43fd37d155a5cadba56c
Liam Ostrander <liamostrander0@gmail.com>
2025-12-01 14:05:55 -0500

    fixed jawn

--- dust/Assets/Scripts/Combat/WeaponStats.cs
+++ dust/Assets/Scripts/Combat/WeaponStats.cs
@@ -3,16 +3,32 @@ using UnityEngine;
 
 public class WeaponStats : MonoBehaviour
 {
+    [Header("Animations")]
     public AnimationClip slashAnimation;
-    public AudioClip slashSound; 
     public AnimationClip dashSliceAnimation;
+
+    [Header("SFX")]
+    public AudioClip slashSound; 
     public AudioClip dashSliceSound; 
+
+    [Header("General Stats")]
     public float damage = 25f;
     public float knockback = 5f;
     public float hitstopDuration = 0.08f;
+
+    [Header("Weapon Type")]
+    public bool isOrbStaff = false;
     public bool isRanged = false;
     public GameObject projectilePrefab;
     public float projectileSpeed = 12f;
     public Transform projectileSpawnPoint;
+    
+    public GameObject orbPrefab;
+    public float orbHoverHeight = 1.2f;
+    public float orbDetectRadius = 6f;
+    public float orbSpeed = 12f;
+    public float orbExplosionRadius = 2f;
+    public float orbExplosionDamage = 30f;
+
 }
 
